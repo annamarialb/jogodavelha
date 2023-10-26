7779cc4113d5350a453598ce0da98a4b5023851a
Milena Azevedo <147430235+milenazevedo@users.noreply.github.com>
2023-10-26 08:42:45 -0300

    Update jogodavelha_teste.c

--- jogodavelha_teste.c
+++ jogodavelha_teste.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
 
 int main(){
+    setlocale(LC_ALL,"portuguese");
 
     // estrutura de dados?
     int l, c, linha, coluna, jogador, ganhou, jogadas, opcao;
