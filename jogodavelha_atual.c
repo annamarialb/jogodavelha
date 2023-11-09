5e35a99824a0c3f8b3e5d20cb6f8e1bd088415a1
Maria Eduarda <142428634+iduarda@users.noreply.github.com>
2023-11-09 09:57:44 -0300

    adicionado creditos, e limpar o menu

--- jogodavelha_atual.c
+++ jogodavelha_atual.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
+#include <string.h>
 
-/* Fazendo um jogo: o jogo da velha, para o projeto de Técnicas e Desenvolvimento de Algortimos */
+/* Fazendo um jogo: o jogo da velha, para o projeto de TÃ©cnicas e Desenvolvimento de Algortimos */
 
 void menu (int *numero);
 
@@ -12,6 +13,8 @@ int main(int argc, char *argv[]) {
 	
 	// estrutura de dados
 	int l, c, linha, coluna, jogador, ganhou, jogadas, opcao, numero = 0;
+	char jogador1[50];
+	char jogador2[50];
 	char jogo [3][3], voltar;
 	
 	// menu do jogo
@@ -21,6 +24,11 @@ int main(int argc, char *argv[]) {
 
 	switch(numero){
 		case 1:{
+			printf("Digite o nome do jogador 1: ");
+			scanf("%s", &jogador1);
+			
+			printf("\nDigite o nome do jogador 2: ");
+			scanf("%s", &jogador2);
 		do{
 			jogador = 1;
 			ganhou = 0;
@@ -57,7 +65,7 @@ int main(int argc, char *argv[]) {
 				
 				// ler cordenadas
 				do{
-					printf("\n JOGADOR 1 = o\nJOGADOR 2 = x\n");
+					printf("\nJOGADOR 1 (%s) = o\nJOGADOR 2 (%s) = x\n",jogador1,jogador2);
 					printf("\nJOGADOR %d: Digite a linha e a coluna que queira jogar: ", jogador);
 					scanf("%d%d", &linha, &coluna);
 				}while(linha<0 || linha>2 || coluna<0 || coluna>2 || jogo[linha][coluna]!=' ');
@@ -76,14 +84,14 @@ int main(int argc, char *argv[]) {
 				if(jogo[0][0] == 'o' && jogo[0][1] == 'o' && jogo[0][2] == 'o' ||
 				   jogo[1][0] == 'o' && jogo[1][1] == 'o' && jogo[1][2] == 'o' ||
 				   jogo[2][0] == 'o' && jogo[2][1] == 'o' && jogo[2][2] == 'o'){
-				   	printf("\nO jogador 1 venceu!\n");
+				   	printf("\nO jogador(a) %s venceu!\n",jogador1);
 				   	ganhou = 1;
 				}
 				
 				if(jogo[0][0] == 'x' && jogo[0][1] == 'x' && jogo[0][2] == 'x' ||
 				   jogo[1][0] == 'x' && jogo[1][1] == 'x' && jogo[1][2] == 'x' ||
 				   jogo[2][0] == 'x' && jogo[2][1] == 'x' && jogo[2][2] == 'x'){
-				   	printf("\nO jogador 2 venceu!\n");
+				   	printf("\nO jogador(a) %s venceu!\n",jogador2);
 				   	ganhou = 1;
 				}
 				
@@ -91,34 +99,34 @@ int main(int argc, char *argv[]) {
 				if(jogo[0][0] == 'o' && jogo[1][0] == 'o' && jogo[2][0] == 'o' ||
 				   jogo[0][1] == 'o' && jogo[1][1] == 'o' && jogo[2][1] == 'o' ||
 				   jogo[0][2] == 'o' && jogo[1][2] == 'o' && jogo[2][2] == 'o'){
-					printf("\nO jogador 1 venceu!\n");
+					printf("\nO jogador(a) %s venceu!\n",jogador1);
 					ganhou = 1;	
 				}
 				
 				if(jogo[0][0] == 'x' && jogo[1][0] == 'x' && jogo[2][0] == 'x' ||
 				   jogo[0][1] == 'x' && jogo[1][1] == 'x' && jogo[2][1] == 'x' ||
 				   jogo[0][2] == 'x' && jogo[1][2] == 'x' && jogo[2][2] == 'x'){
-					printf("\nO jogador 2 venceu!\n");
+					printf("\nO jogador(a) %s venceu!\n",jogador2);
 					ganhou = 1;   	
 				}
 				
 				// ganhar na diagonal principal
 				if(jogo[0][0] == 'o' && jogo[1][1] == 'o' && jogo[2][2] == 'o'){
-					printf("\nO jogador 1 venceu!\n"); 
+					printf("\nO jogador(a) %s venceu!\n",jogador1); 
 					ganhou = 1;
 				}
 				if(jogo[0][0] == 'x' && jogo[1][1] == 'x' && jogo[2][2] == 'x'){
-					printf("\nO jogador 2 venceu!\n"); 
+					printf("\nO jogador(a) %s venceu!\n",jogador2); 
 					ganhou = 1;
 				}
 				
-				// ganhar na diagonal secundária
+				// ganhar na diagonal secundÃ¡ria
 				if(jogo[0][2] == 'o' && jogo[1][1] == 'o' && jogo[2][0] == 'o'){
-					printf("\nO jogador 1 venceu!\n");
+					printf("\nO jogador(a) %s venceu!\n");
 					ganhou = 1;
 				}
 				if(jogo[0][2] == 'x' && jogo[1][1] == 'x' && jogo[2][0] == 'x'){
-					printf("\nO jogador 2 venceu!\n");
+					printf("\nO jogador(a) %s venceu!\n");
 					ganhou = 1;
 				}
 			
@@ -146,10 +154,20 @@ int main(int argc, char *argv[]) {
 	  	
 	  	case 2:{
 	  		printf("okay");
+	  		
+	  		if (solicitarRetornoAoMenu()) {
+            menu(&numero);
+            } else {
+            printf("Ok\n");
+            numero = 4; 
+            }
 		break;
 		}
 		
 		case 3:{
+			printf("\n\tDesenvolvido por Anna Maria, Maria Eduarda e Milena Azevedo\n");
+			printf("\n\tAlunos do UNIPE - Ciencia da Computacao\n");
+			printf("\n\tTurma: Tecnicas e Desenvolvimento de Algoritmos - 1B 2023.2\n");
 			
 			if (solicitarRetornoAoMenu()) {
             menu(&numero);
@@ -174,6 +192,7 @@ int main(int argc, char *argv[]) {
 }
 
 void menu (int *numero){
+	system("cls");
 	printf("\n\tJOGO DA VELHA\n\n");
 	printf("1 - JOGAR\n");
 	printf("2 - VER RANKING\n");
@@ -188,9 +207,9 @@ void menu (int *numero){
     char voltar;
     printf("\n Deseja retornar ao menu?\n");
     printf(" 'S' - Sim \n");
-    printf(" 'N' - N�o \n");
-    printf("Digite a op��o desejada: ");
-    // Limpa o buffer de entrada antes de ler a escolha do usu�rio
+    printf(" 'N' - Não \n");
+    printf("Digite a opção desejada: ");
+    // Limpa o buffer de entrada antes de ler a escolha do usuário
     while ((getchar()) != '\n');
     scanf("%c", &voltar);
     // Limpa qualquer coisa que tenha ficado no buffer de entrada
@@ -199,6 +218,6 @@ void menu (int *numero){
     if(voltar == 'S' || voltar == 's') {
         return 1; // Retorna 1 para sim
     } else {
-        return 0; // Retorna 0 para n�o
+        return 0; // Retorna 0 para não
     }
 }
